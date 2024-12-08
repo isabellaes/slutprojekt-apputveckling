fcae407ccf503946dc35a5bfde284c7f0dfba438
isabellaes <borjessonisabella@gmail.com>
2024-12-08 12:38:46 +0100

    update date time picker

--- components/DateTimePicker.tsx
+++ components/DateTimePicker.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { Pressable, SafeAreaView, View } from "react-native";
 import {
   Text,
   Button,
@@ -7,6 +7,7 @@ import {
   Portal,
   useTheme,
   TextInput,
+  IconButton,
 } from "react-native-paper";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { Item } from "../utils/Types";
@@ -50,26 +51,40 @@ export const DateTimePickerExample = () => {
     <SafeAreaView>
       <Portal>
         <Modal
-          style={{ padding: 5, backgroundColor: theme.colors.background }}
+          contentContainerStyle={{
+            flex: 1,
+          }}
           visible={visible}
           onDismiss={() => setVisible(false)}
         >
-          <Text>{date.toISOString()}</Text>
-          <TextInput
-            style={{ width: 200, marginBottom: 5 }}
-            label={"Titel Lista"}
-            onChangeText={(text) => setTitle(text)}
-          />
-          <Button onPress={() => showMode("date")}>Välj datum</Button>
-          <Button onPress={() => showMode("time")}>Välj tid</Button>
-          <Button
-            onPress={() => {
-              saveActivity();
-              setVisible(false);
+          <View
+            style={{
+              padding: 10,
+              backgroundColor: theme.colors.background,
+              gap: 10,
             }}
           >
-            Spara
-          </Button>
+            <IconButton icon={"close"} onPress={() => setVisible(false)} />
+
+            <Text style={{ textAlign: "center" }}>
+              Datum och tid: {date.toISOString().split("T")[0]}{" "}
+              {date.toISOString().split("T")[1].substring(0, 5)}
+            </Text>
+            <TextInput
+              label={"Titel"}
+              onChangeText={(text) => setTitle(text)}
+            />
+            <Button onPress={() => showMode("date")}>Välj datum</Button>
+            <Button onPress={() => showMode("time")}>Välj tid</Button>
+            <Button
+              onPress={() => {
+                saveActivity();
+                setVisible(false);
+              }}
+            >
+              Spara
+            </Button>
+          </View>
         </Modal>
       </Portal>
       <Button onPress={() => setVisible(true)}>Lägg till ny händelse</Button>
